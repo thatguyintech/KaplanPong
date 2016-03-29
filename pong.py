ff73de8f1d7dfb324f214983c5e58c2d7aa5f089
Albert Hu <alberthu@berkeley.edu>
2016-03-28 19:33:03 -0700

    3/28 - Created player_paddle with OOP that can move up and down and stops at the ends of the screen

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 #Credit the Invent With Python book (http://inventwithpython.com)
 #for doRectsOverlap and isPointInsideRect functions
-
+import pygame
 #used to detect collisions in our game
 def doRectsOverlap(rect1, rect2):
     for a, b in [(rect1, rect2), (rect2, rect1)]:
@@ -22,14 +22,26 @@ def isPointInsideRect(x, y, rect):
 
 import pygame, sys, random
 pygame.init()
-screen = pygame.display.set_mode([640,480])
+screen = pygame.display.set_mode([1000,1000])
 black = [0, 0, 0]
 
 #the game's variables
 #SECTION 1 - YOUR CODE HERE FOR CREATING VARIABLES AND FUNCTIONS
-player_paddle = pygame.Rect([10,10,10,50])
-player_color = [255, 255, 255]
+class Paddle():
+    def __init__(self,color,pos,height,width):
+        self.pos = pos
+        self.height = height
+        self.width = width
+        self.color = color
 
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.width, self.height)
+    def upward(self):
+        self.pos[1] -= 10
+    def down(self):
+        self.pos[1] -= -10
+pygame.key.set_repeat(10,10)
+player_paddle = Paddle((0,0,250),[0,0],150,30)
 running = True
 #game loop
 while running:
@@ -38,15 +50,17 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEMOTION:
-            print
+            print ("mouse moved")
             #SECTION 2 - YOUR CODE HERE FOR WHEN THE MOUSE IS MOVED
-    
-        pressed = pygame.key.get_pressed()
+
+        pressed =  pygame.key.get_pressed()
         if event.type == pygame.KEYDOWN:
             if pressed[pygame.K_UP]:
-                player_paddle.move_ip(0,-10) 
+                if player_paddle.pos[1] > 0:
+                    player_paddle.upward()
             if pressed[pygame.K_DOWN]:
-                player_paddle.move_ip(0,10)
+                if player_paddle.pos[1] < 1000-player_paddle.height:
+                    player_paddle.down()
             #SECTION 3 - YOUR CODE HERE FOR WHEN A KEY IS PRESSED
 
     #pause for 20 milliseconds
@@ -56,10 +70,11 @@ while running:
 
     #logic for moving everything in the game and checking collisions
     #SECTION 4 - YOUR CODE HERE FOR CHANGING VARIABLES AND CHECKING FOR COLLISIONS
-    pygame.draw.rect(screen, player_color, player_paddle)
     
     #draw everything on the screen
     #SECTION 5 - YOUR CODE HERE FOR DRAWING EVERYTHING
+    pygame.draw.rect(screen,player_paddle.color,player_paddle.rect())
+
     
     #update the entire display
     pygame.display.update()
